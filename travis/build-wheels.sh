2b01753309be1f8bb292a650314b366fc9d8100c
Hobson Lane <github@totalgood.com>
2018-02-25 17:09:42 -0800

    add travis stuff and examples

---
+++ travis/build-wheels.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e -x
+
+# Install a system package required by our library
+yum install -y atlas-devel
+yum install -y portaudio-devel
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    "${PYBIN}/pip" install -r /io/dev-requirements.txt
+    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    auditwheel repair "$whl" -w /io/wheelhouse/
+done
+
